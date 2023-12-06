f7f67e4d0061b92a9c2d537ee20ab66b3495ed83
Alessio-Di-Domenico <alessio.661@hotmail.it>
2023-12-06 15:32:30 +0100

    contract factory

--- ethereum-basic-storage/deploy.js
+++ ethereum-basic-storage/deploy.js
@@ -1,6 +1,16 @@
 const ethers = require("ethers");
+const fs = require("fs");
 
-async function main() {}
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider("http://0.0.0.0:8545");
+  const wallet = new ethers.Wallet("privateKey", provider);
+  const abi = fs.readFileSync("./Storage_sol_Storage.abi", "utf8");
+  const binary = fs.readFileSync("./Storage_sol_Storage.bin", "utf8");
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying contract...");
+  const contract = await contractFactory.deploy();
+  console.log("Contract address: ", contract.address);
+}
 
 main()
   .then(() => process.exit(0))
